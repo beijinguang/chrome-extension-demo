9f5d1031191d158baeb1a69391b18c3b0fac59e1
wangjinguang6 <wangjinguang6@jd.com>
2022-04-19 16:52:29 +0800

    修改时间增加右键菜单

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
+chrome.action.setBadgeBackgroundColor(
+    { color: [0, 255, 0, 0] }
+);
 
+// chrome.action.setBadgeText({text: 'new'});
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
@@ -11,5 +15,22 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
 
 
 chrome.bookmarks.getTree(function (bookmarkArray) {
-    console.log(JSON.stringify(bookmarkArray));
+    //console.log(JSON.stringify(bookmarkArray));
 });
+
+chrome.contextMenus.create({
+    id: 'foo',
+    title: 'first',
+    contexts: ['selection']
+})
+
+function contextClick(info, tab) {
+    const { menuItemId } = info
+
+    if (menuItemId === 'foo') {
+        // do something
+        console.log("foo click ")
+    }
+}
+
+chrome.contextMenus.onClicked.addListener(contextClick)
